1050a9797130d1722e3db2f0e0fd11f6ee7704d4
robsaffell <robsaffell@northwesternmutual.com>
2017-05-18 12:15:38 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('expres'),
+	app		= expres (),
+	bp		= require('body-parser'),
+	path	= require('path'),
+	port 	= 8000;
+
+app.use(bp.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname + '/client')));
+app.set('views', path.join(__dirname + '/client/templates'));
+app.set('view engine', 'ejs');
+
+require('./server/config/routes.js')(app);
+
+app.listen(port, function(){
+	console.log('working');
+})
