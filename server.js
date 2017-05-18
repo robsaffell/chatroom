13c5a1f3b9169bcb10ff0503d5f106b8a1e245e5
gei6939 <patrickgeisler@northwesternmutual.com>
2017-05-18 12:26:55 -0500

    new

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('expres'),
-	app		= expres (),
+var express = require('express'),
+	app		= express (),
 	bp		= require('body-parser'),
 	path	= require('path'),
 	port 	= 8000;
@@ -8,7 +8,13 @@ app.use(bp.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname + '/client')));
 app.set('views', path.join(__dirname + '/client/templates'));
 app.set('view engine', 'ejs');
+io.on('connection', function(client) {
+    console.log('Connected...');
+    client.on('join', function(data) {
+        console.log(data);
+    });
 
+});
 require('./server/config/routes.js')(app);
 
 app.listen(port, function(){
